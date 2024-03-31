bdb414031fa18b90568ef372feced19f89b38c5b
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2024-03-31 11:45:02 +0000

    Taskdone

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a classroom.
+ */
+export default class Classroom {
+  /**
+   * Creates a new Classroom.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import Classroom from './0-classroom';
+
+/**
+ * Creates an array of classrooms with a specific size.
+ * @returns An array of classrooms.
+ */
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new Classroom(size));
+}
