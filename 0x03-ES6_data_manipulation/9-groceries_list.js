11edc7cbfa79c6742ac4fd66d69fdfb650e9018b
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2024-03-31 11:59:51 +0000

    fix

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a map of grocery names and their quantity.
- * @returns {Map<string, Number>} A Map containing grocery names as keys and their quantities as values.
+ * @returns {Map<string, Number>} A Map.
  */
 export default function groceriesList() {
   const values = [
