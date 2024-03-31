e1ed3680b65d0f3fee02271f0d4158a0675535a2
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2024-03-31 12:06:21 +0000

    fix

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -6,7 +6,7 @@
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
     return students.reduce(
-      (prevStudent, curStudent) => prevStudent.id ? prevStudent.id + curStudent.id : prevStudent + curStudent.id,
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
       0,
     );
   }
